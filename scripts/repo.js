f39110ac1c314d81fa750702a8c21a4e932f7bd9
David Swaintek <dswaintek@gmail.com>
2015-12-16 14:41:54 -0800

    Added package.json

--- scripts/repo.js
+++ scripts/repo.js
@@ -6,9 +6,8 @@ repos.requestRepos = function(callback) {
   //TODO: How would you like to fetch your repos?
   $.ajax({
     type: 'GET',
-    url: 'https://api.github.com/users/swaintek/repos' +
-    '?sort=updated',
-    headers: {Authorization: 'token ' + githubToken}
+    url: '/github/users/swaintek/repos' +
+    '?sort=updated'
   }).done(function(data) {
     console.log(data);
     repos.all = data;
